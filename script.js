793881d6987fd3dcaf9fc59eb57c182560c9814c
Poletron <65890946+Poletron@users.noreply.github.com>
2022-09-13 16:12:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function round(num) {
+    var m = Number((Math.abs(num) * 100).toPrecision(15));
+    return Math.round(m) / 100 * Math.sign(num);
+}
+
+function CalcularPrecioSinIvaUSD(precioFinal) {
+    let precioSinIvaUSD = precioFinal / 1.19;
+    return precioSinIvaUSD;
+}
+
+function calcularIva(precioFinal) {
+    let IVA = (CalcularPrecioSinIvaUSD(precioFinal) * 0.16);
+    return IVA;
+}
+
+function calcularIGTF(precioFinal) {
+    let IGTF = (CalcularPrecioSinIvaUSD(precioFinal) * 0.03);
+    return IGTF;
+}
+
+function calcularTotalAIngresarUSD(precioFinal) {
+    let totalAIngresarUSD = CalcularPrecioSinIvaUSD(precioFinal) + calcularIva(precioFinal);
+    return totalAIngresarUSD;
+}
+
+function calcularTotalAIngresarBS(precioFinal,tasaDiaria) {
+    let totalAIngresarBS = calcularTotalAIngresarUSD(precioFinal) * tasaDiaria;
+    return totalAIngresarBS;
+}
+
+document.getElementById('boton-calcular').addEventListener('click',function () {
+    let montoFinal = document.getElementById('monto-final').value;
+    let tasaDiaria = document.getElementById('tasa-diaria').value;
+    document.getElementById('total-a-ingresar').innerText = round(calcularTotalAIngresarBS(montoFinal,tasaDiaria)) + ' BS';
+    document.getElementById('resultado-igtf').innerText = round(calcularIGTF(montoFinal)*tasaDiaria) + ' BS';
+})
+
+console.log(calcularTotalAIngresarBS(5,5));
+console.log(calcularIGTF(150));
